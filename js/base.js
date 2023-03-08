e0e7f4bef3a6d11ba3e80009afa20139eb60be0f
Davin Haley <dhaley4@student.cvtc.edu>
2023-03-08 13:55:57 -0600

    spinner slider

--- js/base.js
+++ js/base.js
@@ -1,10 +1,11 @@
 function rotateDiv() {
   let div = document.getElementById('spinTime'); 
+  let slider = document.getElementById('spinSlider')
 
   let rotation = 0;
   setInterval(function() {
     div.style.transform = `rotate(${rotation}deg)`;
-    rotation += 1;
+    rotation += parseInt(slider.value) * 1.5;
   }, 25);
 }
 
